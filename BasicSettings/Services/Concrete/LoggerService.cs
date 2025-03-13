d601c98034e1f83dd6411c914e1efc8d76d26c0b
Sherxon2001 <holmuminocsherxon@gmail.com>
2025-03-13 15:27:45 +0500

    log errorlarni yozishda xatolik path chiqaradigan bo'ldi

--- BasicSettings/Services/Concrete/LoggerService.cs
+++ BasicSettings/Services/Concrete/LoggerService.cs
@@ -15,12 +15,37 @@
 
         public void LogError(Exception ex = default, string content = default)
         {
-            _logger.LogError($"{content} {ex?.Message}");
+            var className = GetClassName(ex);
+            var methodName = GetMethodName(ex);
+            var message = $"\n{ex.Message}\nClass: {className}\nMethod: {methodName}\n{ex.InnerException}";
+
+            try
+            {
+                //_service.TelegramBotClient.SendMessage(757851321, message);
+            }
+            catch { }
+            _logger.LogError($"{content} {message}");
         }
 
         public void LogWarning(Exception ex = default, string content = default)
         {
             _logger.LogWarning($"{content} {ex?.Message}");
+        }
+
+        private string GetClassName(Exception ex)
+        {
+            var st = new StackTrace(ex, true);
+            var frame = st.GetFrame(0);
+            var method = frame.GetMethod();
+            return method.DeclaringType.FullName;
+        }
+
+        private string GetMethodName(Exception ex)
+        {
+            var st = new StackTrace(ex, true);
+            var frame = st.GetFrame(0);
+            var method = frame.GetMethod();
+            return method.Name;
         }
     }
 }
